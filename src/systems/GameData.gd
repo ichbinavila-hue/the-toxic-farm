21bb778095224c2b3f438466a608715f9cd20385
ichbinavila-hue <ichbinavila@gmail.com>
2026-09-12 00:46:10 +0200

    Add missing GameData runtime class

---
+++ src/systems/GameData.gd
@@ -0,0 +1,9 @@
+extends Node
+class_name GameData
+
+const SEEDS := {
+    "turnip":{"name":"Nabo","days":4},
+    "carrot":{"name":"Cenoura","days":5},
+    "tomato":{"name":"Tomate","days":7},
+    "corn":{"name":"Milho","days":8}
+}
